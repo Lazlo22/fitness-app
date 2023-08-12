b516f9f5784e095177c62c4e6bdb4af0cfe93439
Lazlo22 <v.prihodski@gmail.com>
2023-08-12 17:37:37 +0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `fitness-app-custom`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
